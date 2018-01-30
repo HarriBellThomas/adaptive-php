87519613bcdd5c6c4a2c77ff9abf783b45d06b96
Harri Bell-Thomas <harribt@live.co.uk>
2018-01-30 17:33:32 +0000

    Weird bug...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,6 @@ openssl aes-256-cbc -K $encrypted_c9ee5b34b6c3_key -iv $encrypted_c9ee5b34b6c3_i
 ls -la && \
 chmod 0700 deploy-hbt-adaptive && \
 ssh-add deploy-hbt-adaptive && \
-scp -i deploy-hbt-adaptive package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
+scp -i deploy-hbt-adaptive package.tgz $REMOTE_USER@$REMOTE_HOST && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh && \
 echo "Ran SCP to production"
