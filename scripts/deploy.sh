6f7e440303edab1d688f297b4fab24347abb01a9
Harri Bell-Thomas <harribt@live.co.uk>
2018-01-30 16:06:16 +0000

    Package up at start of script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,7 @@
-set -x
-
+tar -czf package.tgz $TRAVIS_BUILD_DIR && \
 openssl aes-256-cbc -K $encrypted_c9ee5b34b6c3_key -iv $encrypted_c9ee5b34b6c3_iv -in deploy.enc -out deploy -d && \
 ls -la && \
-chmod 0700 deploy & \\
-tar -czf package.tgz $TRAVIS_BUILD_DIR && \
+chmod 0700 deploy && \
 scp -i deploy package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh && \
 echo "Ran SCP to production"
