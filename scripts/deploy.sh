d9d49590739090af7b307858fab91f64f0b151af
Harri Bell-Thomas <harribt@live.co.uk>
2018-01-30 15:59:06 +0000

    Changes deploy key to 0700

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@ set -x
 
 openssl aes-256-cbc -K $encrypted_c9ee5b34b6c3_key -iv $encrypted_c9ee5b34b6c3_iv -in deploy.enc -out deploy -d
 ls -la
+chmod 0700 deploy
 
 tar -czf package.tgz $TRAVIS_BUILD_DIR && \
 scp -i deploy package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
