5d78f2c2198f177fff5277c40386995a8cfc3d76
Harri Bell-Thomas <harribt@live.co.uk>
2018-01-30 17:19:14 +0000

    Explicityl declared key file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 tar -czf package.tgz $TRAVIS_BUILD_DIR && \
-openssl aes-256-cbc -K $encrypted_c9ee5b34b6c3_key -iv $encrypted_c9ee5b34b6c3_iv -in deploy.enc -out deploy -d && \
+openssl aes-256-cbc -K $encrypted_c9ee5b34b6c3_key -iv $encrypted_c9ee5b34b6c3_iv -in deploy.enc -out deploy-hbt-adaptive -d && \
 ls -la && \
-chmod 0700 deploy && \
-ssh-add deploy && \
-scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
+chmod 0700 deploy-hbt-adaptive && \
+ssh-add deploy-hbt-adaptive && \
+scp -i deploy-hbt-adaptive package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh && \
 echo "Ran SCP to production"
