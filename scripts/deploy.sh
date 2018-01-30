0d31e957074f428483e74b23b1b00ef09c8f659f
Harri Bell-Thomas <harribt@live.co.uk>
2018-01-30 15:26:49 +0000

    Changed travis build path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 set -x
 
-tar -czf package.tgz build && \
+tar -czf package.tgz $TRAVIS_BUILD_DIR && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh && \
 echo "Ran SCP to production"
