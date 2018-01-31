1362a1eef792b0c87d5a793d8d7191ce19caddaf
NiallEgan <negan1998@gmail.com>
2018-01-31 23:58:24 +0000

    Maybe fixed permissions problem in untar.sh

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -5,6 +5,7 @@ cd /var/www && \
 rm -rf html && \
 tar zxvf travis-deploy.tgz -C . && \
 find ./app -not -name '.env' -not -name 'vendor' -delete && \
+chown deploy:www-data app
 cp -r pkg/adaptive-php/src/* app && \
 ln -s app/public html
 cd app && \
