ede9c948fc67eb7e2c852b1c147c20fc540cf641
Harri Bell-Thomas <harribt@live.co.uk>
2018-02-01 12:05:28 +0000

    Edited untar - no longer chown the app directory

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -7,9 +7,9 @@ tar zxvf travis-deploy.tgz -C ./ && \
 cd app && \
 find . ! -name '.env' ! -path './vendor/*' -type f -exec rm -f {} + && \
 cd .. && \
-chown -R deploy:www-data app
+#chown -R deploy:www-data app && \
 cp -r pkg/adaptive-php/src/* app && \
-ln -s app/public html
+ln -s app/public html && \
 cd app && \
 chown -R deploy:www-data storage && \
 chmod -R 775 storage && \
