1d3f6848042944df3daa3017712d6ff3bcbc4deb
NiallEgan <negan1998@gmail.com>
2018-02-01 00:48:16 +0000

    Changed remove

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,8 +4,10 @@ set -x
 cd /var/www && \
 rm -rf html && \
 tar zxvf travis-deploy.tgz -C . && \
-find ./app -not -name '.env' -not -name 'vendor' -delete && \
-chown deploy:www-data app
+cd app && \
+find . ! -name '.env' ! -name 'vendor' -type f -exec rm -f {} + && \
+cd .. && \
+chown -R deploy:www-data app
 cp -r pkg/adaptive-php/src/* app && \
 ln -s app/public html
 cd app && \
