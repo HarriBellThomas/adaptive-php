a40cab615c6b8382cb1ca7a204f1bb1facb12079
NiallEgan <negan1998@gmail.com>
2018-02-01 02:02:57 +0000

    changed some syntax

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -3,13 +3,12 @@ set -x
 
 cd /var/www && \
 rm -rf html && \
-rm -rf pkg && \
 tar zxvf travis-deploy.tgz -C ./ && \
 cd app && \
-find . ! -name '.env' ! -name './vendor/*' -type f -exec rm -f {} + && \
+find . ! -name '.env' ! -name 'vendor' -type f -exec rm -f {} + && \
 cd .. && \
 chown -R deploy:www-data app
-cp -a ./pkg/adaptive-php/src/. app/ && \
+cp -r pkg/adaptive-php/src/* app && \
 ln -s app/public html
 cd app && \
 chown -R deploy:www-data storage && \
