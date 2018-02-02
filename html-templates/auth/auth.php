f246c48f189b9f412159908a05ef4ecf3b440823
Harri Bell-Thomas <harribt@live.co.uk>
2018-02-02 16:49:45 +0000

    Trying document.postMessage

--- html-templates/auth/auth.php
+++ html-templates/auth/auth.php
@@ -11,6 +11,25 @@ if(isset($_GET["success"])) {
 <body>
     <input id="id" type="hidden" value="12345" />
     <h1>Success</h1>
+    <script>
+    window.addEventListener('message', function(event) {
+
+        console.log(event.data);
+        window.parent.postMessage("test-a", '*')
+        return;
+        // IMPORTANT: Check the origin of the data!
+        // if (~event.origin.indexOf('http://yoursite.com')) {
+            // The data has been sent from your site
+
+            // The data sent with postMessage is stored in event.data
+            // console.log(event.data);
+        // } else {
+            // The data hasn't been sent from your site!
+            // Be careful! Do not use it.
+            // return;
+        // }
+    });
+    </script>
 </body>
 </html>
 
