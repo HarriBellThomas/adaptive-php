2c4c0f30dfbaf8bf15452d28ed95870c75739e0f
NiallEgan <negan1998@gmail.com>
2018-02-11 20:26:04 +0000

    Change to untar script

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -5,7 +5,7 @@ cd /var/www && \
 rm -rf html && \
 tar zxvf travis-deploy.tgz -C ./ && \
 cd app && \
-find . ! -name '.env' ! -path './vendor/*' -type f -exec rm -f {} + && \
+find . ! -name '.env' ! -path './vendor/*' ! -path './storage/* '-type f -exec rm -f {} + && \
 cd .. && \
 #chown -R deploy:www-data app && \
 cp -r pkg/adaptive-php/src/* app && \
@@ -20,4 +20,3 @@ rm -rf html-templates/* && \
 cp -r pkg/adaptive-php/html-templates/* html-templates && \
 chown -R deploy:www-data html-templates && \
 chmod -R 775 html-templates
-
