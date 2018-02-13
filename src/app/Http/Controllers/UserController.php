460a4242acad153a8094a7d40acd38441a5cc9d0
NiallEgan <negan1998@gmail.com>
2018-02-13 14:52:52 +0000

    Added graceful failure for no default style

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -15,6 +15,9 @@ class UserController extends Controller
     public function default_style($id)
     {
       $user = User::findOrFail($id);
+      if (count($user->default_style) == 0) {
+        return response()->json("{'err': 'no default'}");
+      }
       $default_style = $user->default_style[0]['style'];
       if ($default_style) {
         return response()->json($default_style, 200);
