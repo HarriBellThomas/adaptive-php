4ccf56e05636bbc0be8c995f3f82ef27a68767d6
Harri Bell-Thomas <harribt@live.co.uk>
2018-03-01 12:46:37 +0000

    More style markup

--- src/resources/views/library/index.blade.php
+++ src/resources/views/library/index.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.master')
 @section('title', 'Style Library')
 @section('header')
-  @parent
-  <link href="{{url('css/show_style.css')}}" rel="stylesheet" type="text/css" media="all" />
+    @parent
+    <link href="{{url('css/show_style.css')}}" rel="stylesheet" type="text/css" media="all" />
+    <style>
+    li.adaptive-style-tag {
+        background-color: green;
+        color: white;
+        font-weight: 700;
+        font-size: 0.8em;
+        padding: 1px 7px;
+        margin: 2px;
+        border-radius: 4px;
+    }
+    </style>
 @endsection
 @section('content')
 
@@ -14,13 +25,20 @@
                 <div class="row">
                     <div class="masonry">
                         <div class="masonry__container">
+
+                            @foreach($styles as $style)
+
                             <div class="col-sm-4 masonry__item">
                                 <div class="card card-1 boxed boxed--sm boxed--border">
+
+                                    <div class="card__body">
+                                        <h3 class="type--bold" style="margin: 5px 0;">Style Title</h3>
+                                        <p>
+                                            Keeping productive in the morning
+                                        </p>
+                                    </div>
                                     <div class="card__top">
                                         <div class="card__avatar">
-                                            <a href="sections-cards.html#">
-                                                <img alt="Image" src="img/avatar-round-1.png">
-                                            </a>
                                             <span>
                                                 <strong>Alyssa Miller</strong>
                                             </span>
@@ -29,152 +47,16 @@
                                             <span>14 mins</span>
                                         </div>
                                     </div>
-                                    <div class="card__body">
-                                        <a href="sections-cards.html#">
-                                            <img src="img/inner-5.jpg" alt="Image" class="border--round">
-                                        </a>
-                                        <p>
-                                            Keeping productive in the morning
-                                            <a href="sections-cards.html#">#workfromanywhere</a>
-                                        </p>
-                                    </div>
                                     <div class="card__bottom">
                                         <ul class="list-inline">
-                                            <li>
-                                                <div class="card__action">
-                                                    <a href="sections-cards.html#">
-                                                        <i class="material-icons">comment</i>
-                                                        <span>10</span>
-                                                    </a>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div class="card__action">
-                                                    <a href="sections-cards.html#">
-                                                        <i class="material-icons">favorite</i>
-                                                        <span>6</span>
-                                                    </a>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div class="card__action">
-                                                    <a href="sections-cards.html#">
-                                                        <i class="material-icons">share</i>
-                                                    </a>
-                                                </div>
-                                            </li>
+                                            <li class="adaptive-style-tag">Tag1</li>
+                                            <li class="adaptive-style-tag">Tag2</li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-sm-4 masonry__item">
-                                <div class="card card-1 boxed boxed--sm boxed--border">
-                                    <div class="card__top">
-                                        <div class="card__avatar">
-                                            <a href="sections-cards.html#">
-                                                <img alt="Image" src="img/avatar-round-2.png">
-                                            </a>
-                                            <span>
-                                                <strong>Jane Maslow</strong>
-                                            </span>
-                                        </div>
-                                        <div class="card__meta">
-                                            <span>23 mins</span>
-                                        </div>
-                                    </div>
-                                    <div class="card__body">
-                                        <a href="sections-cards.html#">
-                                            <img src="img/knowledge-1.jpg" alt="Image" class="border--round">
-                                        </a>
-                                        <p>
-                                            Our workspace for today
-                                            <a href="sections-cards.html#">#lovingstack</a>
-                                        </p>
-                                    </div>
-                                    <div class="card__bottom">
-                                        <ul class="list-inline">
-                                            <li>
-                                                <div class="card__action">
-                                                    <a href="sections-cards.html#">
-                                                        <i class="material-icons">comment</i>
-                                                        <span>4</span>
-                                                    </a>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div class="card__action">
-                                                    <a href="sections-cards.html#">
-                                                        <i class="material-icons">favorite</i>
-                                                        <span>7</span>
-                                                    </a>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div class="card__action">
-                                                    <a href="sections-cards.html#">
-                                                        <i class="material-icons">share</i>
-                                                    </a>
-                                                </div>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-4 masonry__item">
-                                <div class="card card-1 boxed boxed--sm boxed--border">
-                                    <div class="card__top">
-                                        <div class="card__avatar">
-                                            <a href="sections-cards.html#">
-                                                <img alt="Image" src="img/avatar-round-3.png">
-                                            </a>
-                                            <span>
-                                                <strong>Cam Ruiz</strong>
-                                            </span>
-                                        </div>
-                                        <div class="card__meta">
-                                            <span>53 mins</span>
-                                        </div>
-                                    </div>
-                                    <div class="card__body">
-                                        <a href="sections-cards.html#">
-                                            <h4>
-                                                Maintaining productivity in the face of constant distraction
-                                            </h4>
-                                        </a>
-                                        <p>
-                                            The key to focus is multiple distractions
-                                            <a href="sections-cards.html#">#productivity</a>
-                                        </p>
-                                    </div>
-                                    <div class="card__bottom">
-                                        <ul class="list-inline">
-                                            <li>
-                                                <div class="card__action">
-                                                    <a href="sections-cards.html#">
-                                                        <i class="material-icons">comment</i>
-                                                        <span>10</span>
-                                                    </a>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div class="card__action">
-                                                    <a href="sections-cards.html#">
-                                                        <i class="material-icons">favorite</i>
-                                                        <span>6</span>
-                                                    </a>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div class="card__action">
-                                                    <a href="sections-cards.html#">
-                                                        <i class="material-icons">share</i>
-                                                    </a>
-                                                </div>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
+
+                            @endforeach
                         </div>
                         <!--end masonry__container-->
                     </div>
