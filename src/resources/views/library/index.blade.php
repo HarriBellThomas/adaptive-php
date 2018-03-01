846817cbc6a4d7d300c8287fe23f4ccf2a7d9499
Harri Bell-Thomas <harribt@live.co.uk>
2018-03-01 11:58:13 +0000

    Integrated the master layout in the library view

--- src/resources/views/library/index.blade.php
+++ src/resources/views/library/index.blade.php
@@ -1,16 +1,13 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Library</title>
-    </head>
-    <body>
+@extends('layouts.master')
+@section('title', 'Style ' . $style['name'])
+@section('header')
+  @parent
+  <!-- <link href="{{url('css/show_style.css')}}" rel="stylesheet" type="text/css" media="all" /> -->
+@endsection
+@section('content')
 
         @foreach($styles as $style)
         <pre>{{$style}}</pre><hr>
         @endforeach
-  </body>
-</html>
+
+@endsection
