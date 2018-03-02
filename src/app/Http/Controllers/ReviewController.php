cf75f710617b5b6a0d7ff1636ac7414fd8520187
Harri Bell-Thomas <harribt@live.co.uk>
2018-03-02 16:55:05 +0000

    Tweaks

--- src/app/Http/Controllers/ReviewController.php
+++ src/app/Http/Controllers/ReviewController.php
@@ -8,23 +8,22 @@ use Illuminate\Support\Facades\Auth;
 
 
 
-class ReviewController extends Controller
-{
-  public function store(Request $request)
-  {
-    $style = $this->validate(request(), [
-        'rating' => 'required',
-        'review' => 'required'
-    ]);
 
-    $review_object = new Review;
-    $review_object->review = $request->review;
-    $review_object->stars = $request->rating;
+class ReviewController extends Controller {
+    public function store(Request $request) {
+        $style = $this->validate(request(), [
+            'rating' => 'required',
+            'review' => 'required'
+        ]);
 
-    $review_object->user()->associate(Auth::user()->id);
-    $review_object->style()->associate(request()->style_id);
-    $review_object->save();
+        $review_object = new Review;
+        $review_object->review = $request->review;
+        $review_object->stars = $request->rating;
 
-    return back();
-  }
+        $review_object->user()->associate(Auth::user()->id);
+        $review_object->style()->associate(request()->style_id);
+        $review_object->save();
+
+        return back();
+    }
 }
