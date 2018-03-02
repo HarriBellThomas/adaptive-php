c7f410a9063420132da631a7c90f42bece2efd14
Harri Bell-Thomas <harribt@live.co.uk>
2018-03-02 22:54:46 +0000

    Added header to style library

--- src/resources/views/library/index.blade.php
+++ src/resources/views/library/index.blade.php
@@ -1,66 +1,78 @@
 @extends('layouts.master')
 @section('title', 'Style Library')
 @section('header')
-    @parent
-    <link href="{{url('css/show_style.css')}}" rel="stylesheet" type="text/css" media="all" />
-    <style>
-    li.adaptive-style-tag {
-        background-color: green;
-        color: white;
-        font-weight: 700;
-        font-size: 0.8em;
-        padding: 1px 7px !important;
-        margin: 2px !important;
-        border-radius: 4px;
-    }
-    </style>
+@parent
+<link href="{{url('css/show_style.css')}}" rel="stylesheet" type="text/css" media="all" />
+<style>
+li.adaptive-style-tag {
+    background-color: green;
+    color: white;
+    font-weight: 700;
+    font-size: 0.8em;
+    padding: 1px 7px !important;
+    margin: 2px !important;
+    border-radius: 4px;
+}
+</style>
 @endsection
 @section('content')
 
-        <section class=" ">
-            <div class="container">
-                <div class="row">
-                    <div class="masonry">
-                        <div class="masonry__container">
+<section id="page-title" class="text-center cta cta-4 space--xxs border--bottom imagebg" data-gradient-bg='#a8c0ff,#3f2b96,#0f0c29,#302b63'>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>Public Style Library</h1>
+            </div>
+        </div>
+        <!--end of row-->
+    </div>
+    <!--end of container-->
+</section>
 
-                            @foreach($styles as $style)
+<section class=" ">
+    <div class="container">
+        <div class="row">
+            <div class="masonry">
+                <div class="masonry__container">
 
-                            <div class="col-sm-4 masonry__item">
-                                <div class="card card-1 boxed boxed--sm boxed--border">
-                                    <a href="/style/{{$style['id']}}">
-                                        <div class="card__body">
-                                            <h3 class="type--bold" style="margin: 5px 0;">{{$style['name']}}</h3>
-                                        </div>
-                                    </a>
-                                    <div class="card__body" style="padding-bottom: 0;">
-                                        <div class="card__avatar">
-                                            <span>
-                                                <strong>{{ $users[ $style['user_id'] ]['user_name'] }}</strong>
-                                            </span>
-                                        </div>
-                                        <div class="card__meta">
-                                            <span>Rating: {{ $ratings[ $style['user_id'] ] }}</span>
-                                        </div>
-                                    </div>
-                                    <div class="card__bottom">
-                                        <ul class="list-inline">
-                                            @foreach($tags[$style['id']] as $tag)
-                                            <li class="adaptive-style-tag">{{ $tag['tag_name'] }}</li>
-                                            @endforeach
-                                        </ul>
-                                    </div>
+                    @foreach($styles as $style)
+
+                    <div class="col-sm-4 masonry__item">
+                        <div class="card card-1 boxed boxed--sm boxed--border">
+                            <a href="/style/{{$style['id']}}">
+                                <div class="card__body">
+                                    <h3 class="type--bold" style="margin: 5px 0;">{{$style['name']}}</h3>
+                                </div>
+                            </a>
+                            <div class="card__body" style="padding-bottom: 0;">
+                                <div class="card__avatar">
+                                    <span>
+                                        <strong>{{ $users[ $style['user_id'] ]['user_name'] }}</strong>
+                                    </span>
+                                </div>
+                                <div class="card__meta">
+                                    <span>Rating: {{ $ratings[ $style['user_id'] ] }}</span>
                                 </div>
                             </div>
-
-                            @endforeach
+                            <div class="card__bottom">
+                                <ul class="list-inline">
+                                    @foreach($tags[$style['id']] as $tag)
+                                    <li class="adaptive-style-tag">{{ $tag['tag_name'] }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         </div>
-                        <!--end masonry__container-->
                     </div>
-                    <!--end masonry-->
+
+                    @endforeach
                 </div>
-                <!--end of row-->
+                <!--end masonry__container-->
             </div>
-            <!--end of container-->
-        </section>
+            <!--end masonry-->
+        </div>
+        <!--end of row-->
+    </div>
+    <!--end of container-->
+</section>
 
 @endsection
