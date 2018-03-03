d938460bcc525dc8877607fd323498b8f54f2b65
Harri Bell-Thomas <harribt@live.co.uk>
2018-03-03 16:48:36 +0000

    Redo for Home

--- src/app/Http/Controllers/StyleLibraryController.php
+++ src/app/Http/Controllers/StyleLibraryController.php
@@ -21,7 +21,7 @@ class StyleLibraryController extends Controller {
         $tagsMap = [];
         $ratingsMap = [];
 
-        $styles = Style::where('name', '<>', '')->paginate(2);
+        $styles = Style::where('name', '<>', '')->paginate(6);
 
         foreach ($styles as $style) {
             $styleMap[$style['id']] = $style;
