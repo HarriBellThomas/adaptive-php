47d284bb6151f4d49292361c44a7c58f1967d69f
Harri Bell-Thomas <harribt@live.co.uk>
2018-03-03 16:34:43 +0000

    Redo for Home

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Style;
+use App\User;
+use App\Tag;
+use App\Review;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
