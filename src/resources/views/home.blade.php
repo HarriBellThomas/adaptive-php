3c8672d4e95b37f5da1ad0ed171199c1d4fabd26
Harri Bell-Thomas <harribt@live.co.uk>
2018-03-03 00:53:44 +0000

    Removed text from the home view

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -52,12 +52,8 @@ function changeDefault(str) {
 
 <div class="container">
 
-    <h1 class='text-center'> Hello, {{Auth::user()['user_name']}}. Welcome to the Adaptive Web. </h1>
-
     @if(count(Auth::user()->styles) > 0)
-    <h2 class='text-center'> <a href="{{url('style/create')}}">+  Add a new style </a></h2>
     <div class='style-container text-center'>
-        <h2> Here are your styles: </h2>
         @foreach(Auth::user()->styles as $style)
         <div id="style-{{$style['id']}}" class='indivdual-style' style='border: solid; margin: 10px; padding: 10px;'>
             <h3> {{$style['name']}} </h3>
