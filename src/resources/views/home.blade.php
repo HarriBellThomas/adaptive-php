47e7c73f836648ba86a8118b681fbaa36205fe29
Harri Bell-Thomas <harribt@live.co.uk>
2018-03-05 02:09:41 +0000

    JS removal for #_=_

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -4,6 +4,22 @@
 @section('header')
 @parent
 <script>
+if (window.location.hash && window.location.hash === "#_=_") {
+    var scrollV, scrollH, loc = window.location;
+    if ("pushState" in history)
+        history.pushState("", document.title, loc.pathname + loc.search);
+    else {
+        // Prevent scrolling by storing the page's current scroll offset
+        scrollV = document.body.scrollTop;
+        scrollH = document.body.scrollLeft;
+
+        loc.hash = "";
+
+        // Restore the scroll offset, should be flicker free
+        document.body.scrollTop = scrollV;
+        document.body.scrollLeft = scrollH;
+    }
+    }
 function changeDefault(str) {
     if (str == "") {
         return;
